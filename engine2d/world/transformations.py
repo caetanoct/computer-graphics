d87361438d0cf975f83773c3d882ab3540c70f9a
caetanoct <caetanoct@github.com>
2021-02-10 20:30:26 -0300

    first implementation of transformations

---
+++ engine2d/world/transformations.py
@@ -0,0 +1,37 @@
+# we use the matrixes below to multiply with the points and do the transformation that always uses the same algorithm
+# example: transform (point, matrix)
+# we can do a sequence of transformations as a sequence of matrix multiplications (in order) and transform the object with the resultant matrix
+# example: translate the center of the object to the origin rotate the object and translate it back to where it was
+# remember: object center is the arithmetic average of the coordinates
+#---------------------------------------------------------------#
+# translation [x' y' 1] = [x y 1] *  [1  0  0]					#
+#									 [0  1  0]					#
+#									 [Dx Dy 1]					#
+#---------------------------------------------------------------#
+# scaling [x' y' 1] = [x y 1] * [Sx 0  0]						#
+#								[0  Sy 0]						#
+#								[0  0  1]						#
+#---------------------------------------------------------------#
+# rotation - clockwise  [x' y' 1] = [x y 1] * [cos_ -sin_ 0]	#
+#											  [sin_  cos_ 0]	#
+#											  [0     0    1]	#
+#---------------------------------------------------------------#
+#
+# TODO: implement translation of objects, scaling (without deslocating), rotations (around the world, around the object, around an arbitrary point)
+#
+from geometry import Point
+import numpy
+# general transforming function, we can use this to any kind of transformation given the right matrix
+def transform(point: Point, matrix):
+	if (len(matrix)>3):
+		print("ERROR: given matrix has more than 3 lines")
+	if (len(matrix[0])>3):
+		print("ERROR: given matrix has more than 3 columns")
+	matrix_1=[point.x,point.y,1]
+	matrix_2=matrix
+	matrix_result=numpy.matmul(matrix_1,matrix_2)
+	return Point(matrix_result[0],matrix_result[1])
+# translates given point in Dx,Dy
+def translation(point: Point, Dx, Dy):
+	translation_matrix=[[1,0,0], [0,1,0],[Dx,Dy,1]]
+	return transform(point,translation_matrix)
